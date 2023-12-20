8f47719c0e3f7b06ffb351accbe8744a5d90179c
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2023-12-20 01:25:58 -0300

    Added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt-BR" className={inter.variable}>
+      <body>{children}</body>
     </html>
   )
 }
