e691618013c96c16d11e909dfe5c845e746caa05
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2023-12-20 01:50:08 -0300

    Added Header layout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -6,9 +6,9 @@ export default function StoreLayout({
   children: React.ReactNode
 }) {
   return (
-    <>
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
       <Header />
       {children}
-    </>
+    </div>
   )
 }
