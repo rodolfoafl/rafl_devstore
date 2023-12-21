3f56dd5b74c87dc0ddf1d52a3ded12bbe977a9f9
Rodolfo Leal <27327749+rodolfoafl@users.noreply.github.com>
2023-12-21 01:30:55 -0300

    Added api route to fetch product by slug

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,10 @@
 import data from '../data.json'
 
-export async function GET() {
+export async function GET(request: Request) {
+  if (request.method !== 'GET') {
+    return Response.json({ message: 'Method not allowed' }, { status: 405 })
+  }
+
   await new Promise((resolve) => setTimeout(resolve, 1000))
 
   const featuredProducts = data.products.filter((product) => product.featured)
